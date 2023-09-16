a5d20ae3d95793be2cb5e8243a007ff04187c6d3
Fabiano Moraes <142937939+Fab-Moraes@users.noreply.github.com>
2023-09-16 07:02:17 -0300

    Update manDeStrings01.py
    Foi acrescentado mais uma função do f-string
    -> Formatar Strings

--- manDeStrings.py
+++ manDeStrings.py
@@ -14,3 +14,13 @@ print("Olá, me chamo {3}. Eu tenho {2} anos de idade,trabalho como {1} e estou 
 
 #  f-string
 print(f"Olá, me chama {nome}. Eu tenho {idade} anos de idade, trabalho como {profissao} e estou matriculado no curso de {linguagem}.")
+
+
+#  Formatar strings com f-string
+#  EX
+
+PI = 3.14159
+
+print(f"Valor de PI: {PI:.2f}")
+#  ou
+print(f"Valor de PI: {PI:10.2f}")
