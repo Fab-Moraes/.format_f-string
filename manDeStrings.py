ebeff7a14aa1eff456cf3588f9ea1e10626e866a
Fabiano Moraes <142937939+Fab-Moraes@users.noreply.github.com>
2023-09-16 06:34:50 -0300

    .format_f-string

---
+++ manDeStrings.py
@@ -0,0 +1,16 @@
+nome = "Fabiano Moraes"
+idade = 39
+profissao = "Programador"
+linguagem = "Python"
+
+
+#  Metodo Format
+print("Olá, me chamo {}. Eu tenho {} anos de idade,trabalho como {} e estou matriculado no curso de {}."
+      .format(nome, idade, profissao, linguagem))
+
+#  Tbm posso buscar pela posição
+print("Olá, me chamo {3}. Eu tenho {2} anos de idade,trabalho como {1} e estou matriculado no curso de {0}." 
+      .format(nome, idade, profissao, linguagem))
+
+#  f-string
+print(f"Olá, me chama {nome}. Eu tenho {idade} anos de idade, trabalho como {profissao} e estou matriculado no curso de {linguagem}.")
